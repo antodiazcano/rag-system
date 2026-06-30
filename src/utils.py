99b9d848a0344b612089ae4c109f1b31985434b2
antodiazcano <adiazcano00@gmail.com>
2026-06-30 13:55:28 +0200

    modified image

--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,8 @@
 """Script to define some utilities used."""
 
 from pinecone import Index, Pinecone
-from sentence_transformers import SentenceTransformer
 
 from src.config import config
-
-
-def embed_chunks(
-    chunks: list[str], embedding_model: str = "all-MiniLM-L6-v2"
-) -> list[list[float]]:
-    """Embeds the chunks.
-
-    Args:
-        chunks: Chunks of text to embed.
-        embedding_model: Model used to embed the chunks.
-
-    Returns:
-        Embedded chunks.
-    """
-
-    model = SentenceTransformer(embedding_model)
-    return model.encode(chunks, normalize_embeddings=True).tolist()
 
 
 def get_index_vector_db() -> Index:
